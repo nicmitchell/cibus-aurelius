1e2d8fb04d4a6ad7e7441795c2eeee10b2067b08
Nic Mitchell <nic@nicmitchell.com>
2017-01-06 00:02:36 -0800

    added desserts

---
+++ src/data/dessert.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    name: 'Fried Cream',
+    desc: 'Breaded and fried Cream of Wheat squares with a splash of orange, cinnamon, & cardamom',
+    time: '4 hours',
+    image: 'image.jpg'
+  }, {
+    name: 'Ginataan',
+    desc: 'Warm coconut milk & coconut cream base overflowing with taro, ube, jackfruit, macapuno, and tapioca pearls',
+    time: '1 hour',
+    image: 'image.jpg'
+  }, {
+    name: 'Warm Creme Brulee',
+    desc: 'Classic custard-like dessert with vanilla bean flavor and carmelized sugar. Served warm, as the only way it should be',
+    time: '1.5 hours',
+    image: 'image.jpg'
+  }, {
+    name: 'Grape Spiced Tartlet with Goat Cheese',
+    desc: 'Muscadine grapes cooked in sugar and spices balanced with goat cheese served in flaky crust',
+    time: '1 hour',
+    image: 'image.jpg'
+  }, {
+    name: 'Mint Mountain',
+    desc: 'Chocolate chip cookies topped with Andes mints served with Talenti mint chip gelato',
+    time: '20 minutes',
+    image: 'image.jpg'
+  }, {
+    name: 'Sweet Sticky Rice Bar',
+    desc: 'Sweet sticky jasmine rice filled with macapuno steamed in a banana leaf',
+    time: '24 hours',
+    image: 'images.jpg'
+  }
+];
