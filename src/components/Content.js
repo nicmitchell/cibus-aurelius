635aa49112f6d7f16d33f58850ee56b38e996290
Nic Mitchell <nic@nicmitchell.com>
2017-01-07 21:04:36 -0800

    updated grid to use columns (masonry type layout)

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import { Grid, Row } from 'react-bootstrap';
+import { Grid, Col } from 'react-bootstrap';
 import MenuItem from './MenuItem';
 
 class Content extends React.Component {
+  constructor(props){
+    super(props);
+
+    let total = props.data.length;
+    let columnLength = parseInt(total / 3, 10);
+    let remainder = total % 3;
+    let column1End = (remainder) ? columnLength + 1 : columnLength;
+    let column2End = ((remainder > 1) ? columnLength + 1 : columnLength) * 2;
+ 
+    this.col1 = props.data.slice(0, column1End);
+    this.col2 = props.data.slice(column1End, column2End);
+    this.col3 = props.data.slice(column2End);
+
+  }
   render() {
     return(
       <Grid>
-        <Row>
+        <Col md={4}>
           { 
-            this.props.data.map((item, idx) => {
+            this.col1.map((item, idx) => {
               return <MenuItem { ...item } key={ idx }/>
             })
           }
-        </Row>
+        </Col>
+        <Col md={4}>
+          { 
+            this.col2.map((item, idx) => {
+              return <MenuItem { ...item } key={ idx }/>
+            })
+          }
+        </Col>
+        <Col md={4}>
+          { 
+            this.col3.map((item, idx) => {
+              return <MenuItem { ...item } key={ idx }/>
+            })
+          }
+        </Col>
       </Grid>
     )
   }
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ItemOptions from './ItemOptions';
-import { Col, Image, Glyphicon } from 'react-bootstrap';
+import { Image, Glyphicon } from 'react-bootstrap';
 
 class MenuItem extends React.Component {
   render() {
     return(
-      <Col md={4}>
-        <div className="menu-card">
-          <Image responsive src="/images/home-entree.jpg"/>
-          <div className="bottom">
-            <h4 className="">{ this.props.name }</h4>
-            <p>{ this.props.desc }</p>
-            <p className="prep-time"><Glyphicon glyph="time"/> { this.props.time }
-            { this.props.options && <ItemOptions options={ this.props.options }/> }
-            </p>
-          </div>
+      <div className="menu-card">
+        <Image responsive src="/images/home-entree.jpg"/>
+        <div className="bottom">
+          <h4 className="">{ this.props.name }</h4>
+          <p>{ this.props.desc }</p>
+          <p className="prep-time"><Glyphicon glyph="time"/> { this.props.time }
+          { this.props.options && <ItemOptions options={ this.props.options }/> }
+          </p>
         </div>
-      </Col>
+      </div>
     );
   }
 }
