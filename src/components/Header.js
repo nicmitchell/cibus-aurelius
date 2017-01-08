d2c556b335a01373476131c810c75e0585fc7376
Nic Mitchell <nic@nicmitchell.com>
2017-01-07 20:20:04 -0800

    formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Grid, Row, Col, Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 class Header extends React.Component {
   render() {
     return (
-            <Navbar inverse collapseOnSelect className="nolist">
-              <Navbar.Header>
-                <Navbar.Brand>
-                  <LinkContainer to="/"><NavItem eventKey={1}><span>Cibus Aurelius</span></NavItem></LinkContainer>
-                </Navbar.Brand>
-              </Navbar.Header>
-              <Navbar.Toggle />
-              <Navbar.Collapse>
-                <Nav pullRight>
-                  <LinkContainer to="/entree"><NavItem eventKey={2}>Entrees</NavItem></LinkContainer>
-                  <LinkContainer to="/light"><NavItem eventKey={3}>Light Fare</NavItem></LinkContainer>
-                  <LinkContainer to="/dessert"><NavItem eventKey={4}>Dessert</NavItem></LinkContainer>
-                </Nav>        
-              </Navbar.Collapse>
-              <div className="clear"></div>
-            </Navbar>
+      <Navbar inverse collapseOnSelect className="nolist">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <LinkContainer to="/"><NavItem eventKey={1}><span>Cibus Aurelius</span></NavItem></LinkContainer>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/entree"><NavItem eventKey={2}>Entrees</NavItem></LinkContainer>
+            <LinkContainer to="/light"><NavItem eventKey={3}>Light Fare</NavItem></LinkContainer>
+            <LinkContainer to="/dessert"><NavItem eventKey={4}>Dessert</NavItem></LinkContainer>
+          </Nav>        
+        </Navbar.Collapse>
+        <div className="clear"></div>
+      </Navbar>
     )
   }
 }
