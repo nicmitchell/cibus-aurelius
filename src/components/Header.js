8855bf898d895573b0a2b6398558bbb1ca380c0e
Nic Mitchell <nic@nicmitchell.com>
2017-02-24 22:56:12 -0800

    html formatting in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,26 @@ export default class Header extends React.Component {
       <Navbar inverse collapseOnSelect className="nolist">
         <Navbar.Header>
           <Navbar.Brand>
-            <LinkContainer to="/"><NavItem eventKey={1}><Glyphicon glyph="grain"/><span className="title">Cibus Aurelius</span></NavItem></LinkContainer>
+            <LinkContainer to="/">
+              <NavItem eventKey={1}>
+                <Glyphicon glyph="grain"/>
+                <span className="title">Cibus Aurelius</span>
+              </NavItem>
+            </LinkContainer>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/entree"><NavItem eventKey={2}>Entrees</NavItem></LinkContainer>
-            <LinkContainer to="/light"><NavItem eventKey={3}>Light Fare</NavItem></LinkContainer>
-            <LinkContainer to="/dessert"><NavItem eventKey={4}>Dessert</NavItem></LinkContainer>
+            <LinkContainer to="/entree">
+              <NavItem eventKey={2}>Entrees</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/light">
+              <NavItem eventKey={3}>Light Fare</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/dessert">
+              <NavItem eventKey={4}>Dessert</NavItem>
+            </LinkContainer>
           </Nav>        
         </Navbar.Collapse>
       </Navbar>
