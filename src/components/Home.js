dfe0c8d2d5956dc5b8a1a6006467ccb50405e787
Nic Mitchell <nic@nicmitchell.com>
2017-02-27 20:44:26 -0800

    restyled Home page with sliding panels (desktop only)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,28 @@
 import React from 'react';
-import { Grid, Row, Col, Image, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import config from '../firebaseConfig';
+import { Link } from 'react-router';
 
 export default class Home extends React.Component {
   render() {
-    const imgURL = config.menuImgBaseURL;
     return (
-      <Grid className="nolist">
-        <Row>
-          <Col md={4}>
-            <LinkContainer to="/entree">
-              <NavItem eventKey={1}>
-                <h1>Entrees</h1>
-                <Image responsive src={ `${ imgURL }joes-special-700.jpg?alt=media` } />
-              </NavItem>
-            </LinkContainer>
-          </Col>
-          <Col md={4}>
-            <LinkContainer to="/light">
-              <NavItem eventKey={2}>
-                <h1>Light Fare</h1>
-                <Image responsive src={ `${ imgURL }fresh-froot-700.jpg?alt=media` }/>
-              </NavItem>
-            </LinkContainer>
-          </Col>
-          <Col md={4}>
-            <LinkContainer to="/dessert">
-              <NavItem eventKey={3}>
-                <h1>Desserts</h1>
-                <Image responsive src={ `${ imgURL }mint-mountain-700.jpg?alt=media` }/>
-              </NavItem>
-            </LinkContainer>
-          </Col>
-        </Row>
-      </Grid>
+      <div className="nolist home">
+        <div className="flex-container">
+          <Link to="/entree" className="flex-item entree">
+            <li>
+              <h1>Entrees</h1>
+            </li>
+          </Link>
+          <Link to="/light" className="flex-item light">
+            <li>
+              <h1>Light Fare</h1>
+            </li>
+          </Link>
+          <Link to="/dessert" className="flex-item dessert">
+            <li>
+              <h1>Desserts</h1>
+            </li>
+          </Link>
+        </div>
+      </div>
     )
   }
 }
